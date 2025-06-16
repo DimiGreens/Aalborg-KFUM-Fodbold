d41bdb1d07acd1849d01e19d182902e838b38b53
Emilia-v-p19 <emilia@pretsch.dk>
2025-06-16 11:50:15 +0200

    fix js
    on click er nu taget ud af html og skrevet ind i js

--- assets/js/maalsaetninger.js
+++ assets/js/maalsaetninger.js
@@ -1,25 +1,84 @@
-function aabenOrdenlighed() {
-  document.getElementById("ordenlighedinfo").style.display = "block";
-}
-//naar man cliker paa korted ordenlighed saa aendre ordenlighedinfo style til block saa man kan se den//
+document.getElementById('ordenlighed').addEventListener('click', ordFunction);
 
-function lukOrdenlighed() {
-  document.getElementById("ordenlighedinfo").style.display = "none";
-}
-//naar man cliker paa luk knappen saa bliver ordenlighedinfo til none igen og kan ikke ses
- 
-function aabenFaellesskab() {
-  document.getElementById("faellesskabinfo").style.display = "block";
+function ordFunction() {
+  document.body.innerHTML +=(
+    `
+    <div class="ordenlighedPopup popupCardMaal">
+      <article class="ordenlighedIndhold maalIndehold">
+        <img
+          src="./assets/pictures/picMaalsaetninger/ordenlighedBillede.png"
+          alt="Ordenlighed billede" />
+        <h3>Ordenlighed</h3>
+        <p>
+          Vi respekterer hinanden, medspillere som modspillere samt dommere,
+          så fodbold bliver en god oplevelse. Vi forventer at alle forældre
+          overholder DBU’s forældreråd, og trænerne overholder “Den gode
+          træner” (se “Den blå tråd”) og altid husker på, at vi
+          repræsenterer Aalborg KFUM i træning og kamp.
+        </p>
+        <button class="lukKnap">Luk</button>
+      </article>
+    </div>`
+  );
+  document
+    .querySelector('.ordenlighedPopup .lukKnap')
+    .addEventListener('click', (e) => e.currentTarget.closest('.ordenlighedPopup').remove());
 }
 
-function lukFaellesskab() {
-  document.getElementById("faellesskabinfo").style.display = "none";
+//Faelleskab 
+document.getElementById('faellesskab').addEventListener('click', faelFunction);
+
+function faelFunction() {
+  document.body.innerHTML +=(
+    `<div class="faellesskabPopup popupCardMaal">
+          <article class="faellesskabIndhold maalIndehold">
+            <img
+              src="./assets/pictures/picMaalsaetninger/faellesskabBillede.png"
+              alt="Fællesskab billede"
+            />
+            <h3>Fællesskab</h3>
+            <p>
+              Vi lægger vægt på kammeratskab og sammenhold, og vi har fokus på
+              at skabe et godt sportsligt og socialt miljø. Foreningen er i sig
+              selv et fællesskab. Alle yder selv en aktiv indsats for at styrke
+              foreningen og fællesskabet og engagerer sig som frivillig i at få
+              fællesskabet til at vokse. Foreningen er større end holdet – og
+              holdet er større end individet.
+            </p>
+            <button class="lukKnap" onclick="lukFaellesskab()">Luk</button>
+          </article>
+        </div>`
+  );
+  document
+    .querySelector('.faellesskabPopup .lukKnap')
+    .addEventListener('click', (e) => e.currentTarget.closest('.faellesskabPopup').remove());
 }
 
-function aabenUdvikling() {
-  document.getElementById("udviklinginfo").style.display = "block";
-}
+//Udvikling
+document.getElementById('udvikling').addEventListener('click', udvFunction);
 
-function lukUdvikling() {
-  document.getElementById("udviklinginfo").style.display = "none";
+function udvFunction() {
+  document.body.innerHTML +=(
+    `<div class="udviklingPopup popupCardMaal">
+          <article class="udviklingIndhold maalIndehold">
+            <img
+              src="./assets/pictures/picMaalsaetninger/udviklingBillede.png"
+              alt="Udvikling billede"
+            />
+            <h3>Udvikling</h3>
+            <p>
+              Vi tilstræber et miljø, hvor vi udfordrer hinanden – menneskeligt
+              og sportsligt – og yder alle en fair behandling. Vi har fokus på
+              det mentale (trivsel, glæde, motivation og kompetence) i forhold
+              til udvikling af holdet, den enkelte spiller, samt fokus på, at vi
+              kontinuerligt udvikler klubben med det formål at fastholde flest
+              muligt i fodbolden.
+            </p>
+            <button class="lukKnap" onclick="lukUdvikling()">Luk</button>
+          </article>
+        </div>`
+  );
+  document
+    .querySelector('.udviklingPopup .lukKnap')
+    .addEventListener('click', (e) => e.currentTarget.closest('.udviklingPopup').remove());
 }
